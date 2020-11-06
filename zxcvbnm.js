b13043595600624b4090758c40171e69fb349fe4
sa.lubenin <sa.lubenin@tensor.ru>
2020-11-06 23:47:50 +0300

    check email

--- zxcvbnm.js
+++ zxcvbnm.js
@@ -3,9 +3,6 @@ if (document.location.hash.indexOf('#congratulations:') === 0) {
     if (em) {
         let keyb = document.querySelector('#keyblock');
         if (keyb) {
-            keyb.querySelector('.chowc').innerHTML = 'Спасибо!';
-            keyb.querySelector('.showt').style.display = 'none';
-            keyb.querySelector('.showb').innerHTML = 'ясно, спасибо ;-)';
             keyb.querySelector('p.keyinfo').innerHTML = 'На Ваш e-mail <br> <h4>' + em + '</h4> был выслан ключ. <br> Если оплата была успешной, он придет в течение ближайших минут. <br>Если не пришло - проверьте на всякий случай папку со спамом.';
             $('#keyblock').modal('show');
         }        
